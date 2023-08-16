8fab6494dcf07d2b4097601374ef4645350ff05b
domster704 <domster704@mail.ru>
2023-08-16 17:24:06 +0300

    add new dots

--- script/generateViewsConfig.js
+++ script/generateViewsConfig.js
@@ -6,7 +6,7 @@ const JOV_VIEWS = {
         "subPosition": ["Middle Engineer"],
         "companyName": "Янтарь+",
         "companyURL": "https://amberizh.ru/",
-        "about": "Разработка мобильного сервиса по оформлению заказов для торговых представителей, просмотру своей статистики и задолженности",
+        "about": "Разработка мобильного сервиса по оформлению заказов для торговых представителей, просмотру своей статистики и задолженности.",
         "skills": ["Android", "Java", "MySQL", "XML", "FTP", "DBF"]
     }, {
         "date": "июнь — июль 2023",
@@ -14,7 +14,7 @@ const JOV_VIEWS = {
         "subPosition": ["Junior Engineer"],
         "companyName": "ПРОНЕТКОМ",
         "companyURL": "https://pronetcom.ru/",
-        "about": "Поддержание сущесвтующего проекта, code-reviewing, написание SQL-запросов, мобильная разработка",
+        "about": "Поддержание сущесвтующего проекта, code-reviewing, написание SQL-запросов, мобильная разработка.",
         "skills": ["JavaScript", "HTML", "CSS", "Cordova", "SVN", "PostgreSQL", "Node.js", "RESTful", "SFA системы"]
     }].reverse()
 }
@@ -26,35 +26,35 @@ const PROJECT_VIEWS = {
         "projectName": "Онлайн-магазин на Android",
         "subPosition": [],
         "projectURL": "https://github.com/domster704/MARS-work",
-        "about": "Онлайн-магазин для ограниченного круга лиц в сфере косметики и парфюмерии, написанный на Java с помощью AndroidAPI",
+        "about": "Онлайн-магазин для ограниченного круга лиц в сфере косметики и парфюмерии, написанный на Java с помощью AndroidAPI.",
         "skills": ["Android", "Java", "MySQL", "XML", "FTP", "DBF"]
     }, {
         "image": "res/image/siteSQB.png",
         "projectName": "Кредит онлайн на карту",
         "subPosition": [],
         "projectURL": "https://domster704.github.io/CreditOnline_SQB_FrontEnd/",
-        "about": "Веб-страница для компании, занимающейся кредитами",
+        "about": "Веб-страница для компании, занимающейся кредитами.",
         "skills": ["HTML", "CSS", "JavaScript", "AdaptiveDesign"]
     }, {
         "image": "res/image/RehabCenter.png",
         "projectName": "Реабилитационный центр",
         "subPosition": [],
         "projectURL": "https://domster704.github.io/RehabCenter_FrontEnd/",
-        "about": "Веб-страница для реабилитационного центра",
+        "about": "Веб-страница для реабилитационного центра.",
         "skills": ["HTML", "CSS", "JavaScript", "AdaptiveDesign"]
     }, {
         "image": "res/image/LCT.png",
         "projectName": "Расчет стоимости недвижимости",
         "subPosition": [],
         "projectURL": "https://github.com/domster704/flaskLCT",
-        "about": "Сервис автоматизирует процесс расчета стоимости квартир и позволяет ускорить процедуру выдачи экспертных заключений о стоимости квартир и избежать сделок, при которых имущество продается по цене ниже рыночной",
+        "about": "Сервис автоматизирует процесс расчета стоимости квартир и позволяет ускорить процедуру выдачи экспертных заключений о стоимости квартир и избежать сделок, при которых имущество продается по цене ниже рыночной.",
         "skills": ["Python", "Flask", "JWT-token", "YandexAPI", "Flutter", "HTML", "SQL"]
     }, {
         "image": "res/image/PBS.png",
         "projectName": "PolarBearSearch",
         "subPosition": [],
         "projectURL": "https://github.com/domster704/PolarBearServer",
-        "about": "Приложение по поиску белых медведей в Арктике по снимкам с самолёта",
+        "about": "Приложение по поиску белых медведей в Арктике по снимкам с самолёта.",
         "skills": ["Python", "Flask", "OpenCV", "C#", "Windows Forms"]
     }].reverse()
 }
