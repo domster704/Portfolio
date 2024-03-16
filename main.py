306d15eb3017ffe558eb6144cddca76ccd9ce094
domster704 <domster704@mail.ru>
2024-03-16 13:37:24 +0300

    make server

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ def projectList():
     return render_template('projectList.html')
 
 
-app.run('0.0.0.0', port=8000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
